7ca6c04ed8891b8d712ee381522832c4a3669ede
thuhina <thuhina.t@techversantinfo.com>
2023-11-08 16:11:16 +0530

    add a space in createstudent page dummy

--- local/createstudent/createstudent.php
+++ local/createstudent/createstudent.php
@@ -15,6 +15,7 @@ $PAGE->set_title($strnewclass);
 $mform=new createstudent_form();
 echo $OUTPUT->header();
 
+
 $returnurl = $CFG->wwwroot.'/local/dashboard/dashboardadmin.php';
 if ($mform->is_cancelled()) {
     redirect($returnurl);
