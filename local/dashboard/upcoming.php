6b2aef668700ca49357546b86cfb5d620be909e2
Sujin <sujinkumar1025@gmail.com>
2023-11-15 11:34:31 +0530

    upcoming updates

--- local/dashboard/upcoming.php
+++ local/dashboard/upcoming.php
@@ -8,17 +8,20 @@ require_once($CFG->libdir . '/mustache/src/Mustache/Autoloader.php');
 Mustache_Autoloader::register();
 
 
-$template4 = file_get_contents($CFG->dirroot . '/local/dashboard/templates/upcoming.mustache');
+$templateAdmin = file_get_contents($CFG->dirroot . '/local/dashboard/templates/upcoming.mustache');
+$templateUser= file_get_contents($CFG->dirroot . '/local/dashboard/templates/upcomings.mustache');
 
 
 // require_once($CFG->dirroot.'/local/createstudent/demo.html');
-global $class,$CFG;
+global $class,$CFG,$USER;
 $context = context_system::instance();
+require_login();
 // $classid = $class->id;
 
 $linkurl = new moodle_url('/local/dashboard/upcoming.php');
 
 $PAGE->set_context($context);
+
 $strnewclass= 'Student Dashboard';
 
 $PAGE->set_url('/local/dashboard/upcoming.php');
@@ -26,12 +29,16 @@ $PAGE->set_url('/local/dashboard/upcoming.php');
 $PAGE->set_title($strnewclass);
 
 echo $OUTPUT->header();
-$mustache = new Mustache_Engine();
 
-echo $mustache->render($template4, $data);
-
+if (has_capability('moodle/site:config', $context)) {
+    // User is a site admin, display admin template
+    $mustache = new Mustache_Engine();
+    echo $mustache->render($templateAdmin, $data);
+} else {
+    // User is a regular user, display user template
+    $mustache = new Mustache_Engine();
+    echo $mustache->render($templateUser, $data);
+}
 
 echo $OUTPUT->footer();
-
-
 ?>
