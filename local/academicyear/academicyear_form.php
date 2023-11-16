79bf56c2b982165157af4d01c20771316f8063c9
thuhina <thuhina.t@techversantinfo.com>
2023-11-16 11:41:08 +0530

    view academic yearlist

--- local/academicyear/academicyear_form.php
+++ local/academicyear/academicyear_form.php
@@ -1,16 +1,5 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Academic year creation</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="/school/local/css/style.css" />
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
-</head>
 <?php
 
 // This file is part of Moodle - http://moodle.org/
@@ -46,75 +35,50 @@ require_login();
 
 class academicyear_form extends moodleform {
     function definition() {
-       // $userid  = optional_param('userid', 0, PARAM_INT);
+
         $urlto=$CFG->wwwroot.'/local/academicyear/academicyear.php';
-        global $USER, $CFG, $DB;
-        $table = new html_table();
-
+        global $USER, $CFG, $COURSE, $DB;
         $mform = $this->_form;
-        $mform->addElement('html', '<div class="form-wrap">
-        <div class="cointainer">
-            <div class="col-md-10 col-10 mx-auto col-lg-6">
-                <div class="form-card card">
-                    <h4>Academic Year creation</h4> 
-                    <div class="form-group">
-                    <label>Select Academic Start Year for the Application</label>');
 
-                    $mform->addElement('date_selector', 'timestart', ' ',array(
-                        'startyear' => 2004, 
-                        'stopyear'  => 2050,
-                        'timezone'  => 99,
-                        'optional'  => false
-                    ));
-
-
-                    $mform->addElement('html', ' </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label>Select Academic End Year for the Application</label>');
-
-                                    $mform->addElement('date_selector', 'timefinish','' ,array(
-                                        'startyear' => 2005, 
-                                        'stopyear'  => 2055,
-                                        'timezone'  => 99,
-                                        'optional'  => false
-                                    ));
-                                    $mform->addElement('html', '</div>
-                        </div>
-                    </div>');
-        // $this->add_action_buttons();
-        // $this->add_action_buttons(array( 
-        //     'submit' => array(
-        //     'name' => 'submit',
-        //     'value' => 'Submit',
-        //     'class' => 'btn btn-primary mt-3 mb-3',
-        // ),
-        //     'cancel' => array(
-        //         'name' => 'cancel',
-        //         'value' => 'Cancel'
-        //     )
-           
-        // ));
+        $mform->addElement('html', '<h2 class="text-center heading mb-5">Academic Year Creation</h2>');
+        $mform->addElement('html', '<div class="container">');
+        $mform->addElement('html', '<div class="form-class">');
         
-        $this->add_action_buttons(false, 'Submit', array('class' => 'btn btn-primary mt-3 mb-3'));
+        // $table = new html_table();
+        $id  = optional_param('id', 0, PARAM_INT);
+        // print_r($id);exit();
 
-        $mform->addElement('html', '<br>');
-        $this->add_action_buttons(false, 'cancel');
+        $mform->addElement('hidden','id',$id);
+        // $mform->addElement('hidden', 'id'); 
+        
+        $mform->addElement('date_selector', 'timestart', 'Academic start',array(
+            'startyear' => 2004, 
+            'stopyear'  => 2050,
+            'timezone'  => 99,
+            'optional'  => false
+        ));
+        $mform->addElement('date_selector', 'timefinish', 'Academic end',array(
+            'startyear' => 2005, 
+            'stopyear'  => 2055,
+            'timezone'  => 99,
+            'optional'  => false
+        ));
 
-        $mform->addElement('html', '</div></div>
-                </div>
-                </div>
-               <!-- <a href="#" class="btn btn-primary mt-3 mb-3">SUBMIT</a>-->
-             
-
-');
-  
-
+        
+ 
        
-        
+      
+        $mform->addElement('html', '</div>');
+
+
+        $this->add_action_buttons();
+        $mform->addElement('html','<a href = "/school/local/academicyear/viewacademicyear.php" style="text-decoration:none">');
+        $mform->addElement('button', 'btn', 'View Academic year  list'); 
+        $mform->addElement('html','</a>');
+        $mform->addElement('html', '</div>');
+  
+       
     }
     
   
 }
-?>
