5f393488b2a6284c5c6b93dcfe5e5f4f3d41e7be
Sujin <sujinkumar1025@gmail.com>
2023-11-20 12:10:44 +0530

    update upcoming_assignment.php

--- local/dashboard/upcoming_assignment.php
+++ local/dashboard/upcoming_assignment.php
@@ -1,13 +1,18 @@
 <?php
 
 require(__DIR__.'/../../config.php');
+require_once($CFG->libdir . '/mustache/src/Mustache/Autoloader.php');
+Mustache_Autoloader::register();
 
-global $DB, $USER;
+$template = file_get_contents($CFG->dirroot . '/local/dashboard/templates/upcoming_assign.mustache');
+
+global $DB, $USER, $OUTPUT, $PAGE;
+
 
 $context = context_system::instance();
 require_login();
 
-$linktext = "Upcoming Assignment";
+//$linktext = "Upcoming Assignment";
 
 $linkurl = new moodle_url('/local/dashboard/upcoming_assignment.php');
 // Print the page header.
@@ -36,15 +41,13 @@ if (empty($enrolled_course_ids)) {
     $params = array_merge($enrolled_course_ids, array($current_date));
 
     $data = $DB->get_records_sql("SELECT * FROM {assign} WHERE course IN ($in_clause) AND duedate >= ?", $params);
+    $mustache = new Mustache_Engine();
+    $tableRows = [];
     if (empty($data)) {
         echo "There are no upcoming assignment for your enrolled courses.";
     } 
     else{
-    $table = new html_table();
-    $table->head = array('Submission Date','Due date', 'Assignment name');
-    $table->data = array();
-    $table->class = '';
-    $table->id = 'asssign_list';
+  
     $context = context_user::instance($USER->id, MUST_EXIST);
 
     foreach ($data as $value) {
@@ -57,36 +60,33 @@ if (empty($enrolled_course_ids)) {
        // Construct the URL for the assignment view page using the course module ID
        $view = $CFG->wwwroot.'/mod/assign/view.php?id='.$cm->id;
 
-       
-
-       
-        // $edit = $CFG->wwwroot.'/local/complaint/edit.php?id='.$value->id;//Edit
-        // $delete = $CFG->wwwroot.'/local/complaint/delete.php?id='.$value->id;//Delete
-        $dates1=date('m/d/Y',$value->allowsubmissionsfromdate);
-        $dates=date('m/d/Y',$value->duedate);
-        $table->data[] = array(
-            $dates1,
-            $dates,
-            $value->name,
-            //<i class="fa-solid fa-eye"></i>
-            //$html = html_writer::link($add, $OUTPUT->pix_icon('i/view','View','moodle'));
-
-            // html_writer::link($add, $OUTPUT->pix_icon('i/eye','View','moodle')), // Add icon
-            // html_writer::link($edit, $OUTPUT->pix_icon('i/edit', 'Edit me', 'moodle')), // Edit icon
-         html_writer::link( $view, $OUTPUT->pix_icon('t/eye',get_string('view'))), // Delete icon
-
-    
+        $dates1=date('d-m-Y',$value->allowsubmissionsfromdate);
+        $dates=date('d-m-Y',$value->duedate);
+        $assign_name=$value->name;
+        $view_icon=html_writer::link(
+            $view,
+            '<span class="custom-icon" aria-label="' . get_string('view') . '">👁️</span>'
+        );
+         $tableRows[] = [
+            'id'=> $id,
+            'Submission_date' => $dates1,
+            'Due_date' => $dates,
+            'Assignment_name' => $assign_name,
+            'View'=>$view_icon,
+        ];
         
 
-        ); 
+        
     }
    // echo $view_icon;
-    echo html_writer::table($table);
-    $backurl = new moodle_url('/local/dashboard/dashboard.php');
-    $backbutton = html_writer::link($backurl, '< Back');
-    echo $backbutton;
+    // echo html_writer::table($table);
+    // $backurl = new moodle_url('/local/dashboard/dashboard.php');
+    // $backbutton = html_writer::link($backurl, '< Back');
+    // echo $backbutton;
 }
 }
-echo $OUTPUT->footer();
+$output = $mustache->render($template, ['tableRows' => $tableRows]);
+echo $output;
 
+echo $OUTPUT->footer();
 ?>
