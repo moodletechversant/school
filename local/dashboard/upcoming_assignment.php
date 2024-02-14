d1dfe31494cfddb874d46c59421cf79ab566f5f2
Sujin <sujinkumar1025@gmail.com>
2024-02-14 12:37:05 +0530

    upcoming aasignment parentview

--- local/dashboard/upcoming_assignment.php
+++ local/dashboard/upcoming_assignment.php
@@ -5,6 +5,7 @@ require_once($CFG->libdir . '/mustache/src/Mustache/Autoloader.php');
 Mustache_Autoloader::register();
 
 $template = file_get_contents($CFG->dirroot . '/local/dashboard/templates/upcoming_assign.mustache');
+$template2 = file_get_contents($CFG->dirroot . '/local/dashboard/templates/upcoming_parentview.mustache');
 
 global $DB, $USER, $OUTPUT, $PAGE;
 
@@ -33,6 +34,70 @@ $userid = $USER->id;
 $current_date = time();
 $enrolled_courses = enrol_get_users_courses($userid);
 $context = context_user::instance($USER->id, MUST_EXIST);
+
+
+$pid = $DB->get_record_sql("SELECT * FROM {parent} WHERE user_id = ?", array($userid));
+
+if ($pid) {
+    $penrolled_courses = enrol_get_users_courses($pid->child_id);
+    $enrolled_course_ids = array();
+foreach ($penrolled_courses as $enrolled_course) {
+    $enrolled_course_ids[] = $enrolled_course->id;
+}
+$in_clause = implode(',', array_fill(0, count($enrolled_course_ids), '?'));
+    $params = array_merge($enrolled_course_ids, array($current_date));
+    //print_r($penrolled_courses);exit();
+    $data = $DB->get_records_sql("SELECT * FROM {assign} WHERE course IN ($in_clause) AND duedate >= ? ORDER BY allowsubmissionsfromdate", $params);
+    $mustache = new Mustache_Engine();
+    $tableRows = [];
+    if (!empty($data)) {
+
+    foreach ($data as $value) {
+       // Get the course module ID from the current row of data (assuming $value is an object)
+       $cmid = $value->id;
+
+       // Load the course module record from the database
+       $cm = get_coursemodule_from_instance('assign', $cmid, 0, false, MUST_EXIST);
+       
+       // Construct the URL for the assignment view page using the course module ID
+       //$view = $CFG->wwwroot.'/mod/assign/view.php?id='.$cm->id;
+
+        $dates1=date('d-m-Y',$value->allowsubmissionsfromdate);
+        $dates=date('d-m-Y',$value->duedate);
+        $assign_name=$value->name;
+        
+         $tableRows[] = [
+            'id'=> $id,
+            'Submission_date' => $dates1,
+            'Due_date' => $dates,
+            'Assignment_name' => $assign_name,
+           // 'View'=>$view_icon,
+        ];
+        
+
+        
+    }
+    } 
+    else{
+        
+       $error= "There are no upcoming assignment";
+       $tableRows[] = [
+        
+        'Submission_date' => $error,
+       
+    ];
+    
+   // echo $view_icon;
+    // echo html_writer::table($table);
+    // $backurl = new moodle_url('/local/dashboard/dashboard.php');
+    // $backbutton = html_writer::link($backurl, '< Back');
+    // echo $backbutton;
+}
+$output = $mustache->render($template2, ['tableRows' => $tableRows,'csspath'=>$csspath,'dashboard'=>$dashboard]);
+echo $output;
+   
+}
+
 
 //print_r($enrolled_courses);exit();
 $enrolled_course_ids = array();
