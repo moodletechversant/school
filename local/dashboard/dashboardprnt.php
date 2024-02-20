6e023f13a8d071af7ed88ec0e05a8a5cc77a4a5d
thuhina <thuhina.t@techversantinfo.com>
2024-02-20 12:35:04 +0530

    dashbaord parent

--- local/dashboard/dashboardprnt.php
+++ local/dashboard/dashboardprnt.php
@@ -11,12 +11,22 @@ $context = context_system::instance();
 $linktext = "Dashboard Parent";
 $linkurl = new moodle_url('/local/dashboard/dashboardprnt.php');
 $css_link= new moodle_url('/local/css/style.css');
+$img_12=new moodle_url('/local/img/ic-12.svg');
+$img_5=new moodle_url('/local/img/ic-5.svg');
+$img_2=new moodle_url('/local/img/ic-2.svg');
+$img_6=new moodle_url('/local/img/ic-6.svg');
+$img_28=new moodle_url('/local/img/ic-28.svg');
+$img_7=new moodle_url('/local/img/ic-7.svg');
+$img_13=new moodle_url('/local/img/ic-13.svg');
+$img_17=new moodle_url('/local/img/ic-17.svg');
+$img_14=new moodle_url('/local/img/ic-14.svg');
+$img_18=new moodle_url('/local/img/ic-18.svg');
 
 $PAGE->set_context($context);
 $PAGE->set_title($strnewclass);
 echo $OUTPUT->header();
 $mustache = new Mustache_Engine();
-echo $mustache->render($template,['css_link'=>$css_link]);
+echo $mustache->render($template,['css_link'=>$css_link,'img_12'=>$img_12,'img_5'=>$img_5,'img_2'=>$img_2,'img_6'=>$img_6,'img_28'=>$img_28,'img_7'=>$img_7,'img_13'=>$img_13,'img_17'=>$img_17,'img_14'=>$img_14,'img_18'=>$img_18]);
 echo $OUTPUT->footer();
 }
 else{
