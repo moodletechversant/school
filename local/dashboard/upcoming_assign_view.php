082441d3026d4784af1d29e1f63a8bec6436e751
Sujin <sujinkumar1025@gmail.com>
2024-04-05 14:26:29 +0530

    updation on upcoming assignment teacher view

--- local/dashboard/upcoming_assign_view.php
+++ local/dashboard/upcoming_assign_view.php
@@ -32,7 +32,7 @@ $img3 = new moodle_url('/local/img/ic-22.svg');
 
 //$upcoming_events = new moodle_url('/local/dashboard/upcoming_events.php');
 $upcomingassignment = new moodle_url('/local/dashboard/classteacherassignmentview.php');
-$upcomingsubjectassign= new moodle_url('/local/dashboard/classteacherassignview.php');
+$upcomingsubjectassign= new moodle_url('/local/dashboard/subteacherassignview.php');
 
  
 $PAGE->set_context($context);
