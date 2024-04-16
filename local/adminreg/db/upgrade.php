5251df54ebd896da7949b998c393ab5dc304ede3
thuhina <thuhina.t@techversantinfo.com>
2024-04-16 16:02:27 +0530

    admin reg upgrade

--- local/adminreg/db/upgrade.php
+++ local/adminreg/db/upgrade.php
@@ -42,7 +42,7 @@ function xmldb_local_adminreg_upgrade($oldversion) {
     //
     // You will also have to create the db/install.xml file by using the XMLDB Editor.
     // Documentation for the XMLDB Editor can be found at {@link https://docs.moodle.org/dev/XMLDB_editor}.
-        if ($oldversion < 2024041506) {
+        if ($oldversion < 2024041507) {
             // Report_user_logins savepoint reached.
             $table = new xmldb_table('admin_registration');
             $table->add_field('id', XMLDB_TYPE_INTEGER, '10',null, XMLDB_NOTNULL, XMLDB_SEQUENCE,null);
@@ -64,6 +64,6 @@ function xmldb_local_adminreg_upgrade($oldversion) {
             }
 
        }
-       upgrade_plugin_savepoint(true, 2024041506, 'local', 'adminreg');
+       upgrade_plugin_savepoint(true, 2024041507, 'local', 'adminreg');
     return true;
 }
